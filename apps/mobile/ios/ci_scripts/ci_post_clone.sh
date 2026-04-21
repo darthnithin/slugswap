4dbd89d98dcd0de434282e1edcfb7a2a465f753c
Nithin <10778861+darthnithin@users.noreply.github.com>
2026-04-21 16:38:10 -0700

    speedups

--- apps/mobile/ios/ci_scripts/ci_post_clone.sh
+++ apps/mobile/ios/ci_scripts/ci_post_clone.sh
@@ -1,19 +1,20 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 system_profiler SPHardwareDataType
 
 echo "Running ci_post_clone.sh"
 date
-# cd out of ios/ci_scripts into main project directory
-cd ../../
+# cd out of ios/ci_scripts into main slugswap directory
+cd ../../../../
 
 # install node
 #HOMEBREW_NO_AUTO_UPDATE=1 brew install node
-time brew install node
+time HOMEBREW_NO_AUTO_UPDATE=1 HOMEBREW_NO_INSTALL_CLEANUP=1 brew install node
 
 # install node modules
-time npm install
+time npm ci --workspace  @slugswap/mobile
 
+cd ./apps/mobile
 # See note above about patching for GetEnv Issue
 #npm i patch-package
 #npx patch-package
