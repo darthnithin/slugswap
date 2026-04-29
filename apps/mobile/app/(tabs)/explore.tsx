bd0a4d9dcfd67fc09c958dcedb028b5e822263fb
Nithin <10778861+darthnithin@users.noreply.github.com>
2026-04-29 14:36:41 -0700

    Add expo maps

--- apps/mobile/app/(tabs)/explore.tsx
+++ apps/mobile/app/(tabs)/explore.tsx
@@ -1,4 +1,4 @@
-import { TabPlaceholderScreen } from '../../components/TabPlaceholderScreen';
+/* import { TabPlaceholderScreen } from '../../components/TabPlaceholderScreen';
 
 export default function ExploreScreen() {
   return (
@@ -9,3 +9,16 @@ export default function ExploreScreen() {
     />
   );
 }
+ */
+ import { AppleMaps, GoogleMaps } from 'expo-maps';
+ import { Platform, Text } from 'react-native';
+
+ export default function ExploreScreen() {
+   if (Platform.OS === 'ios') {
+     return <AppleMaps.View style={{ flex: 1 }} />;
+   } else if (Platform.OS === 'android') {
+     return <GoogleMaps.View style={{ flex: 1 }} />;
+   } else {
+     return <Text>Maps are only available on Android and iOS</Text>;
+   }
+ }
