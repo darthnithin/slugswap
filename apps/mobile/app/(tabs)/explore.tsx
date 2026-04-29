fda43f60580cd96310dd8eec85ed331f737f0403
Nithin <10778861+darthnithin@users.noreply.github.com>
2026-04-29 15:07:41 -0700

    Fix version issue

--- apps/mobile/app/(tabs)/explore.tsx
+++ apps/mobile/app/(tabs)/explore.tsx
@@ -11,13 +11,28 @@ export default function ExploreScreen() {
 }
  */
  import { AppleMaps, GoogleMaps } from 'expo-maps';
+import { AppleMapsMapStyleElevation, AppleMapsMapStyleEmphasis } from 'expo-maps/build/apple/AppleMaps.types';
  import { Platform, Text } from 'react-native';
 
- export default function ExploreScreen() {
-   if (Platform.OS === 'ios') {
-     return <AppleMaps.View style={{ flex: 1 }} />;
+export default function ExploreScreen() {
+  if (Platform.OS === 'ios') {
+    return <AppleMaps.View style={{ flex: 1 }}
+      cameraPosition={
+        {
+          coordinates: {
+            latitude: 36.994,
+            longitude: -122.06,
+          },
+          zoom: 10,
+        }
+      }
+      properties={
+        { elevation: AppleMapsMapStyleElevation.REALISTIC, emphasis: AppleMapsMapStyleEmphasis.AUTOMATIC}
+      }
+     />;
    } else if (Platform.OS === 'android') {
-     return <GoogleMaps.View style={{ flex: 1 }} />;
+     // return <GoogleMaps.View style={{ flex: 1 }} />;
+     return <Text>Maps are only available on iOS</Text>;
    } else {
      return <Text>Maps are only available on Android and iOS</Text>;
    }
